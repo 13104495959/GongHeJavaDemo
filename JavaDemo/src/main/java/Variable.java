930c55073150f1229c9de28f78ca99b4d99fdddd
宫赫 <gonghe0555@gmail.com>
2018-05-29 19:39:25 +0900

    1 frist commit

---
+++ JavaDemo/src/main/java/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+
+    public static void main(String[] argv) {
+        /* 变量
+        * type variableame = value
+        * type 变量类型
+        * variableName 变量名称
+        * value 变量的值
+        * */
+
+        // 多个变量的声明方式1, 不进行初始化
+        int number1, number2, number3;
+        // 多个变量的声明方式2, 进行初始化
+        int number4 = 4, number5 = 5, number6 = 6;
+        // 多个变量的声明方式3, 一部分进行初始化,一部分不进行初始化
+        int number7 = 7, number8;
+        // 单个变量的声明方式4, 初始化
+        int number9 = 9;
+        // 单个变量的声明方式5, 不进行初始化
+        int number10;
+
+    }
+
+}
